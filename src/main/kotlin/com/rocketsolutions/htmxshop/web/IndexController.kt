39aee85b11d0321d1eda82b318d2dd3542f45823
Felix Eckhardt <felix.e@gmx.de>
2024-07-15 11:33:52 +0200

    organize pdp, small clean up for presentation

--- src/main/kotlin/com/rocketsolutions/htmxshop/web/IndexController.kt
+++ src/main/kotlin/com/rocketsolutions/htmxshop/web/IndexController.kt
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import java.util.UUID
 
 @Controller
@@ -16,41 +17,52 @@ import java.util.UUID
 class IndexController(
     private val productService: ProductService,
 ) {
-
     @GetMapping("")
-    fun index(q: String?, model: Model) = "list".also {
-        if (q == null) {
+    fun index(
+        @RequestParam("query") query: String?,
+        model: Model,
+    ): String {
+        if (query == null) {
             model.addAttribute("products", productService.allProducts())
         } else {
-            model.addAttribute("products", productService.searchProduct(q))
+            model.addAttribute("products", productService.searchProduct(query))
         }
-        model.addAttribute("query", q)
+        model.addAttribute("query", query)
         model.addAttribute("basketCount", productService.productsInBasket().size)
+        return "pdp/list"
     }
 
     @GetMapping("/addToBasket/{product-id}")
-    fun addToBasket(@PathVariable("product-id") id: UUID, response: HttpServletResponse) =
-        productService.addToBasket(id)?.let {
-            ResponseEntity.ok(productService.productsInBasket().size.toString())
-                .also { response.addHeader("Hx-Trigger", TRIGGER_BASKET_REFRESH_EVENT) }
-        }
+    fun addToBasket(
+        @PathVariable("product-id") id: UUID,
+        response: HttpServletResponse,
+    ) = productService.addToBasket(id)?.let {
+        ResponseEntity
+            .ok(productService.productsInBasket().size.toString())
+            .also { response.addHeader("Hx-Trigger", TRIGGER_BASKET_REFRESH_EVENT) }
+    }
 
     @GetMapping("/basketCount")
     fun basketCount(): ResponseEntity<String> = ResponseEntity.ok(productService.productsInBasket().size.toString())
 
     @PostMapping("/search")
-    fun search(q: String, model: Model, response: HttpServletResponse) = "component/productlist".also {
+    fun search(
+        query: String,
+        model: Model,
+        response: HttpServletResponse,
+    ) = "pdp/component/productlist".also {
         model.addAttribute(
             "products",
-            productService.allProducts().filter { it.title.lowercase().contains(q.lowercase()) },
+            productService.allProducts().filter { it.title.lowercase().contains(query.lowercase()) },
         )
-        response.addHeader("HX-Push-Url", if (q.isNotBlank()) "/?q=$q" else "/")
+        response.addHeader("HX-Push-Url", if (query.isNotBlank()) "/?q=$query" else "/")
     }
 
     @GetMapping("/more")
-    fun more(model: Model) = "component/productlist".also {
-        model.addAttribute("products", productService.allProducts().shuffled())
-    }
+    fun more(model: Model) =
+        "pdp/component/productlist".also {
+            model.addAttribute("products", productService.allProducts().shuffled())
+        }
 
     companion object {
         const val TRIGGER_BASKET_REFRESH_EVENT = "triggerBasketRefreshEvent"
