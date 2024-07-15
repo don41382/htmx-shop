185bfbd480121babe34e211f093a91e6d587a584
Felix Eckhardt <felix.e@gmx.de>
2024-07-15 12:44:55 +0200

    clean-start presentation (part II)

--- src/main/kotlin/com/rocketsolutions/htmxshop/web/IndexController.kt
+++ src/main/kotlin/com/rocketsolutions/htmxshop/web/IndexController.kt
@@ -1,6 +1,5 @@
 package com.rocketsolutions.htmxshop.web
 
-import com.rocketsolutions.htmxshop.application.service.ProductService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/")
-class IndexController(
-    private val productService: ProductService,
-) {
+class IndexController {
     @GetMapping("/")
     fun index(model: Model): String = "index"
 }
